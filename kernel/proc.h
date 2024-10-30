55b75f2fbc6dafb80520fb325a2b78064c435809
franmauri47 <franmauri47@mi.unc.edu.ar>
2024-10-30 10:40:21 -0300

    description fix 2

--- kernel/proc.h
+++ kernel/proc.h
@@ -91,8 +91,8 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
-  uint priority;               //Proccess priority
-  uint schedCounter;           //Counts the times that the procces was scheduled
+  uint priority;               // Proccess priority
+  uint schedCounter;           // Counts the times that the procces was scheduled
   
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
