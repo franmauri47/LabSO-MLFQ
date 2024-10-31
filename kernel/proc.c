7ae182325f874685c38fa156b3653cb727d401cd
Luis Ramirez <luis.ramirez@unc.edu.ar>
2024-10-31 19:39:33 -0300

    Removed Estrella 3

--- kernel/proc.c
+++ kernel/proc.c
@@ -462,8 +462,6 @@ scheduler(void)
     // Avoid deadlock by ensuring that devices can interrupt.
     intr_on();
 
-    uint foundRunnableProc = 0;
-
     // Iterates the priority levels
     for (uint i = NPRIO-1; i < NPRIO; i--)
     {
@@ -476,8 +474,6 @@ scheduler(void)
         // Validate if process 'p' has priority 'i' and if it's runnable
         if (p->state == RUNNABLE && p->priority == i)
         {
-
-          foundRunnableProc = 1;
           // Validates if process 'p' has been less scheduled that current process
           if (selected_proc == 0 || p->schedCounter < selected_proc->schedCounter)
           {
@@ -531,10 +527,6 @@ scheduler(void)
         }
         cycles_counter = 0;
       }
-    }
-
-    if (!foundRunnableProc) {
-      hlt();
     }
   }
 }
